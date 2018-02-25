28f5c552e0f1555f094c8f103d425631164d4359
Denise Pendleton <dpendl1@hotmail.com>
2018-02-25 01:05:45 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    BRANDS.uniq << brand
 
   end
 
@@ -16,7 +16,7 @@ class Shoe
   end
 
   def brand
-    @brand = BRAND.uniq
+    @brand 
 
   end
 
